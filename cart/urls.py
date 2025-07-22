a4a683f3fca499873846a677fb2272227143efa6
Brittaktb <brittaktb@web.de>
2025-07-22 13:26:27 +0200

    britta branch

---
+++ cart/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = "cart"
+
+urlpatterns = [
+    #path("session/<int:session_id>"/ views.cart_session, name="cart-session"),
+]
---
+++ payments/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = "payments"
+
+urlpatterns = [
+    #path("confirm/<int:booking_id>/", views.pay, name="pay"),
+]
