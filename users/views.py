40f8adebd6a797f93984df75cb0cd4b16c5df988
WimpieRatte <assebliefaanvaar@gmail.com>
2025-07-22 15:06:44 +0200

    updated home view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
 
-# Create your views here.
+def register(request):
+    # Check if the form has been submitted
+    if request.method == 'POST':
+        # Input submitted info into the form
+        form = RegisterForm(request.POST)
+        # Check if form is valid
+        if form.is_valid():
+            form.save()
+            return redirect('shop:home')
+    else:
+        # redirect to a clean registration page
+        form = RegisterForm()
+        return render(request, 'users/register.html', {'form': form})
