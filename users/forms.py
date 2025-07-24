cf69e321681afd00a2cf0a7d9e0f2e15515addd7
WimpieRatte <assebliefaanvaar@gmail.com>
2025-07-24 15:05:54 +0200

    Wimpie stuff

--- users/forms.py
+++ users/forms.py
@@ -8,4 +8,4 @@ class RegisterForm(UserCreationForm):
 
     class Meta:
         model = CustomUser
-        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+        fields = ['first_name', 'last_name', 'email', 'password1', 'password2']
