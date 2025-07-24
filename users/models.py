8c3dc64f737acd1634d3de0f9a01be225977c3ae
WimpieRatte <assebliefaanvaar@gmail.com>
2025-07-24 13:26:21 +0200

    removed username

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
+    username = None
     is_customer = models.BooleanField(default=True)  # adds column
 
     def __str__(self):
