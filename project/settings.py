c199bf7d6e2ccb2ddcdee08e9f143e2594390da2
Brittaktb <brittaktb@web.de>
2025-07-24 13:44:58 +0200

    changes to media_files

--- project/settings.py
+++ project/settings.py
@@ -129,8 +129,8 @@ USE_TZ = True
 STATIC_URL = "static/" 
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'  #browser
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # rootfolder
 
 
 
--- shop/models.py
+++ shop/models.py
@@ -34,7 +34,6 @@ class Product(models.Model):
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    image = models.ImageField(upload_to='product_images/', blank=True, null=True)   # media files
 
     class Meta:
         ordering = ('name',)
