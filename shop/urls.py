da49e7164f93b4f5fc4d0857c2afe9282bb9a7b4
Brittaktb <brittaktb@web.de>
2025-07-25 16:18:15 +0200

    shop/products.html

--- shop/urls.py
+++ shop/urls.py
@@ -5,5 +5,5 @@ app_name = "shop"
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("shop/", views.shop, name="shop"),
+    path('products/', views.product_list, name='product_list'),
 ]
