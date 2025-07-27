a0da224fccfc645b2b81148c29bb6751e53ffce3
Brittaktb <brittaktb@web.de>
2025-07-27 08:47:46 +0200

    save in between

--- shop/views.py
+++ shop/views.py
@@ -9,9 +9,24 @@ def product_list(request):
     products = Product.objects.filter(available=True) # we are filtering to get only available products.
     return render(request, "shop/shop.html", {"products": products})
 
-def product_detail(request):                              # we are filtering to get only available categories.
-    category = Category.objects.filter(availability=True)
-    return render(request, "shop/product_detail.html")
+# def product_detail(request):                              # we are filtering to get only available categories.
+#     category = Category.objects.filter(availability=True)
+#     return render(request, "shop/product_detail.html")
     
 def shop(request):
     return render(request, "shop/shop.html")
+
+
+
+# from .models import Product, CartItem, Cart  # adapt your model names
+# from django.contrib.auth.decorators import login_required
+
+# @login_required
+# def add_to_cart(request, product_id):
+#     product = get_object_or_404(Product, id=product_id, availability=True)
+#     cart, _ = Cart.objects.get_or_create(user=request.user)
+#     item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+#     if not created:
+#         item.quantity +=1
+#         item.save()
+#     return redirect('shop:product_list')
