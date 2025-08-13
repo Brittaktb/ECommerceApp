c2f1d391e1c1e8d474c956c9815fbee12a294d89
WimpieRatte <assebliefaanvaar@gmail.com>
2025-08-13 04:33:39 +0200

    added total

--- cart/urls.py
+++ cart/urls.py
@@ -6,6 +6,6 @@ app_name = "cart"
 urlpatterns = [
     path("", views.view_cart, name="cart"),
     path('add/', views.add_to_cart, name='cart_add'),
-    path('delete/<int:product_id>/', views.delete_from_cart, name='cart_delete'),
+    path('delete/', views.delete_from_cart, name='cart_delete'),
     path('update/', views.update_cart, name='cart_update'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -9,7 +9,8 @@ from django.http import JsonResponse
 def view_cart(request):
     cart = Cart(request)
     cart_products = cart.get_products()
-    return render(request, "cart/cart.html", {"cart_products": cart_products})
+    total_price = cart.get_total_price()
+    return render(request, "cart/cart.html", {"cart_products": cart_products, "total_price": total_price})
 
 def add_to_cart(request):
     # initialise the cart object (it will load whatever existing cart already exists, if one does)
@@ -37,12 +38,28 @@ def add_to_cart(request):
 
     # return render(request, "cart/add-to-cart.html", {"product": product})
 
-def delete_from_cart(request, product_id):
-    return redirect("view_cart")
-    # product = get_object_or_404(Product, id=product_id)
-    # cart = Cart(request)
-    # cart.remove(product)
-    # return redirect("view_cart")
+def delete_from_cart(request):
+    # initialise the cart object
+    cart = Cart(request)
+    # test for POST
+    if request.POST.get('action') == 'post':
+        # get stuff
+        product_id = int(request.POST.get('product_id'))
+
+        # lookup product in database
+        product = get_object_or_404(Product, id=product_id)
+
+        # save to session
+        cart.remove(product=product)
+
+        # get cart quantity
+        cart_quantity = cart.__len__()
+
+        # Return response
+        # return JsonResponse({'product_id': product_id})
+        return JsonResponse({'qty': cart_quantity})
+    else:
+        return JsonResponse({'status': False})
 
 def update_cart(request):
     # initialise the cart object
